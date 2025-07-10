097ba17eab1ee4587f04cf35513788ecfcb32468
arraakularavind <arraakularavind>
2025-07-10 07:50:16 +0000

    user b

--- ETL_PROCESS_FINAL.py
+++ ETL_PROCESS_FINAL.py
@@ -128,6 +128,11 @@ target_field=['ResponseId','Age','Employment','EdLevel','Country','YearsCode','Y
 
 # COMMAND ----------
 
+def user_B():
+    print("User B modify")
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## Expected Fields 
 # MAGIC
