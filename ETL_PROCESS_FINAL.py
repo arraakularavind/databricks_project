d47be66e60d490fe4bfb5d5c23d3ad5cbcefe43e
arraakularavind <arraakularavind>
2025-07-10 05:12:17 +0000

    User A change

--- ETL_PROCESS_FINAL.py
+++ ETL_PROCESS_FINAL.py
@@ -128,6 +128,18 @@ target_field=['ResponseId','Age','Employment','EdLevel','Country','YearsCode','Y
 
 # COMMAND ----------
 
+def critical_section():
+    print("User A also working in cell 6 meant for B")
+    pass
+
+# COMMAND ----------
+
+def User_A():
+    print("User A cell")
+    pass
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## Expected Fields 
 # MAGIC
