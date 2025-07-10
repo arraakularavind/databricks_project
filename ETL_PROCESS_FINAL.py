59d5d9cdc9780477052a06c75eebe1b7e254b2e2
arraakularavind <arraakularavind>
2025-07-10 02:27:17 +0000

    common portion

--- ETL_PROCESS_FINAL.py
+++ ETL_PROCESS_FINAL.py
@@ -128,6 +128,11 @@ target_field=['ResponseId','Age','Employment','EdLevel','Country','YearsCode','Y
 
 # COMMAND ----------
 
+def critical_section():
+    pass
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## Expected Fields 
 # MAGIC
