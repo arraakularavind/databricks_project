c78438289b31ba08cd67ad9514c3691eb88b98a2
raakularavind03 <raakularavind03>
2025-07-10 07:56:28 +0000

    commit undo

--- ETL_PROCESS_FINAL.py
+++ ETL_PROCESS_FINAL.py
@@ -128,11 +128,6 @@ target_field=['ResponseId','Age','Employment','EdLevel','Country','YearsCode','Y
 
 # COMMAND ----------
 
-def user_B():
-    print("User B modify")
-
-# COMMAND ----------
-
 # MAGIC %md
 # MAGIC ## Expected Fields 
 # MAGIC
