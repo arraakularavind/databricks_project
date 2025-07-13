9dcdca2272d63798314c66db85d4926450441b17
raakularavind03 <raakularavind03>
2025-07-13 12:55:22 +0000

    bug at prod

--- ETL_PROCESS_FINAL.py
+++ ETL_PROCESS_FINAL.py
@@ -128,6 +128,11 @@ target_field=['ResponseId','Age','Employment','EdLevel','Country','YearsCode','Y
 
 # COMMAND ----------
 
+def production_bug():
+    print("production_fixed")
+
+# COMMAND ----------
+
 # MAGIC %md
 # MAGIC ## Expected Fields 
 # MAGIC
